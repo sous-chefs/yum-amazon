6eafcbed0951067bb3a21105e479f9fc2df81f44
Sean OMeara <someara@opscode.com>
2013-12-11 13:46:06 -0500

    pinning yum dep to ~> 3.0

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures yum-amazon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'yum'
+depends 'yum', '~> 3.0'
