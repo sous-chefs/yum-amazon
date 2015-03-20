f9fdc75fa4452ccd39a8c3b815b8e5e162ffa3df
Franklin Hanson <fhanson@gannett.com>
2015-03-20 19:32:53 -0400

    GDAES-684 guard against running outside of ec2

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,44 +15,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-%w{
- amzn-main
- amzn-main-debuginfo
- amzn-nosrc
- amzn-preview
- amzn-preview-debuginfo
- amzn-updates
- amzn-updates-debuginfo
-}.each do |repo|
-  yum_repository repo do
-    description node['yum'][repo]['description']
-    baseurl node['yum'][repo]['baseurl']
-    mirrorlist node['yum'][repo]['mirrorlist']
-    gpgcheck node['yum'][repo]['gpgcheck']
-    gpgkey node['yum'][repo]['gpgkey']
-    enabled node['yum'][repo]['enabled']
-    cost node['yum'][repo]['cost']
-    exclude node['yum'][repo]['exclude']
-    enablegroups node['yum'][repo]['enablegroups']
-    failovermethod node['yum'][repo]['failovermethod']
-    http_caching node['yum'][repo]['http_caching']
-    include_config node['yum'][repo]['include_config']
-    includepkgs node['yum'][repo]['includepkgs']
-    keepalive node['yum'][repo]['keepalive']
-    max_retries node['yum'][repo]['max_retries']
-    metadata_expire node['yum'][repo]['metadata_expire']
-    mirror_expire node['yum'][repo]['mirror_expire']
-    priority node['yum'][repo]['priority']
-    proxy node['yum'][repo]['proxy']
-    proxy_username node['yum'][repo]['proxy_username']
-    proxy_password node['yum'][repo]['proxy_password']
-    repositoryid node['yum'][repo]['repositoryid']
-    sslcacert node['yum'][repo]['sslcacert']
-    sslclientcert node['yum'][repo]['sslclientcert']
-    sslclientkey node['yum'][repo]['sslclientkey']
-    sslverify node['yum'][repo]['sslverify']
-    timeout node['yum'][repo]['timeout']
-    action :create
+if node['cloud'] && node['cloud']['provider']
+  if node['cloud']['provider'] == 'ec2'
+    %w{
+     amzn-main
+     amzn-main-debuginfo
+     amzn-nosrc
+     amzn-preview
+     amzn-preview-debuginfo
+     amzn-updates
+     amzn-updates-debuginfo
+    }.each do |repo|
+      yum_repository repo do
+        description node['yum'][repo]['description']
+        baseurl node['yum'][repo]['baseurl']
+        mirrorlist node['yum'][repo]['mirrorlist']
+        gpgcheck node['yum'][repo]['gpgcheck']
+        gpgkey node['yum'][repo]['gpgkey']
+        enabled node['yum'][repo]['enabled']
+        cost node['yum'][repo]['cost']
+        exclude node['yum'][repo]['exclude']
+        enablegroups node['yum'][repo]['enablegroups']
+        failovermethod node['yum'][repo]['failovermethod']
+        http_caching node['yum'][repo]['http_caching']
+        include_config node['yum'][repo]['include_config']
+        includepkgs node['yum'][repo]['includepkgs']
+        keepalive node['yum'][repo]['keepalive']
+        max_retries node['yum'][repo]['max_retries']
+        metadata_expire node['yum'][repo]['metadata_expire']
+        mirror_expire node['yum'][repo]['mirror_expire']
+        priority node['yum'][repo]['priority']
+        proxy node['yum'][repo]['proxy']
+        proxy_username node['yum'][repo]['proxy_username']
+        proxy_password node['yum'][repo]['proxy_password']
+        repositoryid node['yum'][repo]['repositoryid']
+        sslcacert node['yum'][repo]['sslcacert']
+        sslclientcert node['yum'][repo]['sslclientcert']
+        sslclientkey node['yum'][repo]['sslclientkey']
+        sslverify node['yum'][repo]['sslverify']
+        timeout node['yum'][repo]['timeout']
+        action :create
+      end
+    end
   end
 end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,25 +1,44 @@
+# encoding: utf-8
+require 'chefspec'
 require 'spec_helper'
+require 'fauxhai'
 
 describe 'yum-amazon::default' do
-  context 'yum-amazon::default uses default attributes' do
-    let(:chef_run) { ChefSpec::Runner.new(:step_into => ['yum_repository']).converge(described_recipe) }
-
-    %w{
-      amzn-main
-      amzn-main-debuginfo
-      amzn-nosrc
-      amzn-preview
-      amzn-preview-debuginfo
-      amzn-updates
-      amzn-updates-debuginfo
-    }.each do |repo|
-      it "creates yum_repository[#{repo}]" do
-        expect(chef_run).to create_yum_repository('amzn-main')
-      end
-
-      it "steps into yum_repository and creates template[/etc/yum.repos.d/#{repo}.repo]" do
-        expect(chef_run).to render_file("/etc/yum.repos.d/#{repo}.repo")
-      end
-    end
+  let(:chef_run) { ChefSpec::SoloRunner.new }  
+  
+  it 'creates yum_repository[amzn-main]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-main')
+  end
+  it 'creates yum_repository[amzn-main-debuginfo]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-main-debuginfo')
+  end
+  it 'creates yum_repository[amzn-nosrc]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-nosrc')
+  end
+  it 'creates yum_repository[amzn-preview]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-preview')
+  end
+  it 'creates yum_repository[amzn-preview-debuginfo]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-preview-debuginfo')
+  end
+  it 'creates yum_repository[amzn-updates]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-updates')
+  end
+  it 'creates yum_repository[amzn-updates-debuginfo]' do
+    chef_run.node.set['cloud']['provider'] = 'ec2'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_yum_repository('amzn-updates-debuginfo')
   end
 end
