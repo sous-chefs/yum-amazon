391c2a88fb173476d4395fceabbc6c049023d961
Franklin Hanson <fhanson@gannett.com>
2015-03-20 19:43:57 -0400

    GDAES-684 fix tests

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'yum-amazon'
-maintainer       'Chef'
+name 'yum-amazon'
+maintainer 'Chef'
 maintainer_email 'Sean OMeara <someara@getchef.com>'
-license          'Apache 2.0'
-description      'Installs/Configures yum-amazon'
+license 'Apache 2.0'
+description 'Installs/Configures yum-amazon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version '0.1.4'
 
 depends 'yum', '~> 3.0'
