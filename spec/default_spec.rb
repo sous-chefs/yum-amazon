1d7fce65bfd6c154ae65e3a4d6a73289d8598cab
Franklin Hanson <fhanson@gannett.com>
2015-03-26 17:19:47 -0400

    GDAES-684 update spec testing

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,41 +4,60 @@ require 'spec_helper'
 require 'fauxhai'
 
 describe 'yum-amazon::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.new }  
-  
-  it 'creates yum_repository[amzn-main]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-main')
+  context 'amazon' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['platform'] = 'amazon'
+        node.automatic['platform_version'] = '2014.09'
+      end.converge(described_recipe)
+    end
+    
+    it 'creates yum_repository[amzn-main]' do
+      expect(chef_run).to create_yum_repository('amzn-main')
+    end
+    it 'creates yum_repository[amzn-main-debuginfo]' do
+      expect(chef_run).to create_yum_repository('amzn-main-debuginfo')
+    end
+    it 'creates yum_repository[amzn-nosrc]' do
+      expect(chef_run).to create_yum_repository('amzn-nosrc')
+    end
+    it 'creates yum_repository[amzn-preview]' do
+      expect(chef_run).to create_yum_repository('amzn-preview')
+    end
+    it 'creates yum_repository[amzn-preview-debuginfo]' do
+      expect(chef_run).to create_yum_repository('amzn-preview-debuginfo')
+    end
+    it 'creates yum_repository[amzn-updates]' do
+      expect(chef_run).to create_yum_repository('amzn-updates')
+    end
+    it 'creates yum_repository[amzn-updates-debuginfo]' do
+      expect(chef_run).to create_yum_repository('amzn-updates-debuginfo')
+    end
   end
-  it 'creates yum_repository[amzn-main-debuginfo]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-main-debuginfo')
-  end
-  it 'creates yum_repository[amzn-nosrc]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-nosrc')
-  end
-  it 'creates yum_repository[amzn-preview]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-preview')
-  end
-  it 'creates yum_repository[amzn-preview-debuginfo]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-preview-debuginfo')
-  end
-  it 'creates yum_repository[amzn-updates]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-updates')
-  end
-  it 'creates yum_repository[amzn-updates-debuginfo]' do
-    chef_run.node.set['cloud']['provider'] = 'ec2'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to create_yum_repository('amzn-updates-debuginfo')
+
+  context 'centos' do
+    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }  
+    
+    it 'creates yum_repository[amzn-main]' do
+      expect(chef_run).to_not create_yum_repository('amzn-main')
+    end
+    it 'creates yum_repository[amzn-main-debuginfo]' do
+      expect(chef_run).to_not create_yum_repository('amzn-main-debuginfo')
+    end
+    it 'creates yum_repository[amzn-nosrc]' do
+      expect(chef_run).to_not create_yum_repository('amzn-nosrc')
+    end
+    it 'creates yum_repository[amzn-preview]' do
+      expect(chef_run).to_not create_yum_repository('amzn-preview')
+    end
+    it 'creates yum_repository[amzn-preview-debuginfo]' do
+      expect(chef_run).to_not create_yum_repository('amzn-preview-debuginfo')
+    end
+    it 'creates yum_repository[amzn-updates]' do
+      expect(chef_run).to_not create_yum_repository('amzn-updates')
+    end
+    it 'creates yum_repository[amzn-updates-debuginfo]' do
+      expect(chef_run).to_not create_yum_repository('amzn-updates-debuginfo')
+    end
   end
 end
