ec958c3bd0cc5ba2b9fa0e089fcaab485f009d72
Tim Smith <tsmith@chef.io>
2015-09-07 13:23:34 -0700

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'yum-amazon'
-maintainer 'Chef'
-maintainer_email 'Sean OMeara <someara@getchef.com>'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-amazon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
