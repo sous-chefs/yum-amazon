60d64a4e5e8507a646f27881016c3e30d07f7548
Tim Smith <tsmith84@gmail.com>
2015-09-07 21:45:28 -0700

    Add amazon to metadata and add new supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.5'
 
 depends 'yum', '~> 3.0'
+
+supports 'amazon'
+
+source_url 'https://github.com/chef-cookbooks/yum-amazon' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-amazon/issues' if respond_to?(:issues_url)
