9ddd7fb85696fe9a291edae6eeb0fc425afd0a20
Tim Smith <tsmith84@gmail.com>
2015-09-21 21:55:39 -0700

    Require yum 3.2 not 3.0 and improve description

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name 'yum-amazon'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-amazon'
+description 'Installs and configures the Amazon linux yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.5'
 
-depends 'yum', '~> 3.0'
+depends 'yum', '~> 3.2'
 
 supports 'amazon'
 
