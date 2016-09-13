20f8aff17a342dd8bac75ce1043fc9c2ebe61c9f
Tim Smith <tsmith@chef.io>
2016-09-13 10:04:19 -0700

    Use built in yum_repository resource
    yum_repository is now built into Chef 12.14+. We no longer need the yum
    cookbook to provide this resource. For Chef 12.1-12.13 use the
    compat_resource.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,11 @@ license 'Apache 2.0'
 description 'Installs and configures the Amazon linux yum repository'
 version '1.0.0'
 
-depends 'yum', '>= 3.2', '< 5.0'
+depends 'compat_resource', '>= 12.14.1'
 
 supports 'amazon'
 
 source_url 'https://github.com/chef-cookbooks/yum-amazon'
 issues_url 'https://github.com/chef-cookbooks/yum-amazon/issues'
 
-chef_version '>= 12'
+chef_version '>= 12.1'
