2a77cfefc45b1ed79c7d61b0d4ed000b00bec526
Tim Smith <tsmith@chef.io>
2017-03-21 15:59:16 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'yum-amazon'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures the Amazon linux yum repository'
 version '2.0.1'
 
