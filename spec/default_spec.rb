6f11dabdcef187388f06f56c4e6362c0f47feb19
Tim Smith <tsmith@chef.io>
2017-06-14 10:56:56 -0700

    Fix specs to actually test on amazon
    These were not setup right
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 describe 'yum-amazon::default' do
   context 'amazon' do
-    cached(:chef_run) do
-      ChefSpec::ServerRunner.new do |node|
-        node.automatic['platform'] = 'amazon'
-        node.automatic['platform_version'] = '2014.09'
-      end.converge(described_recipe)
-    end
+    cached(:chef_run) { ChefSpec::SoloRunner.new(platform: 'amazon', version: '2016.09').converge(described_recipe) }
 
     it 'creates yum_repository[amzn-main]' do
       expect(chef_run).to create_yum_repository('amzn-main')
@@ -33,27 +28,27 @@ describe 'yum-amazon::default' do
   end
 
   context 'centos' do
-    cached(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+    cached(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.3.1611').converge(described_recipe) }
 
-    it 'creates yum_repository[amzn-main]' do
+    it 'does not create yum_repository[amzn-main]' do
       expect(chef_run).to_not create_yum_repository('amzn-main')
     end
-    it 'creates yum_repository[amzn-main-debuginfo]' do
+    it 'does not create yum_repository[amzn-main-debuginfo]' do
       expect(chef_run).to_not create_yum_repository('amzn-main-debuginfo')
     end
-    it 'creates yum_repository[amzn-nosrc]' do
+    it 'does not create yum_repository[amzn-nosrc]' do
       expect(chef_run).to_not create_yum_repository('amzn-nosrc')
     end
-    it 'creates yum_repository[amzn-preview]' do
+    it 'does not create yum_repository[amzn-preview]' do
       expect(chef_run).to_not create_yum_repository('amzn-preview')
     end
-    it 'creates yum_repository[amzn-preview-debuginfo]' do
+    it 'does not create yum_repository[amzn-preview-debuginfo]' do
       expect(chef_run).to_not create_yum_repository('amzn-preview-debuginfo')
     end
-    it 'creates yum_repository[amzn-updates]' do
+    it 'does not create yum_repository[amzn-updates]' do
       expect(chef_run).to_not create_yum_repository('amzn-updates')
     end
-    it 'creates yum_repository[amzn-updates-debuginfo]' do
+    it 'does not create yum_repository[amzn-updates-debuginfo]' do
       expect(chef_run).to_not create_yum_repository('amzn-updates-debuginfo')
     end
   end
