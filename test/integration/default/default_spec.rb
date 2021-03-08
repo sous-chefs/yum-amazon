a9c9dc9e84a3c1875ce1622485a028a16c90a339
Ryan <kennedy@osuosl.org>
2021-03-08 12:23:18 -0800

    Add ini test

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -15,3 +15,14 @@ describe yum.repo('amzn2-core-source') do
   it { should exist }
   its('mirrors') { should cmp 'http://amazonlinux.default.amazonaws.com/2/core/latest/SRPMS/mirror.list' }
 end
+
+%w(
+  amzn2-core
+  amzn2-core-debuginfo
+  amzn2-core-source
+).each do |repo|
+  describe ini("/etc/yum.repos.d/#{repo}.repo") do
+    its("#{repo}.gpgcheck") { should cmp 1 }
+    its("#{repo}.gpgkey") { should cmp 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-amazon-linux-2' }
+  end
+end
